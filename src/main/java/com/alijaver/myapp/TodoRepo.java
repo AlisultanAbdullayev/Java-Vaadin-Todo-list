61b04a5b1c1b457c03ac0caac6212cde8f7ff69f
Alisultan Abdullah <47978568+AlisultanAbdullayev@users.noreply.github.com>
2020-07-11 12:48:32 +0300

    master

---
+++ src/main/java/com/alijaver/myapp/TodoRepo.java
@@ -0,0 +1,11 @@
+package com.alijaver.myapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+
+public interface TodoRepo extends JpaRepository<Todo, Long> {
+
+    @Transactional
+    void deleteByDone(boolean done);
+}
